d31bdf2e5373eb49f1c24e351243a8f37293e9d8
shayaaan <42377211+shayanahmedk@users.noreply.github.com>
2024-05-08 07:12:32 +0500

    added chessboard.js

--- chapter-2/chessboard.js
+++ chapter-2/chessboard.js
@@ -2,4 +2,18 @@
 // characters to separate lines. At each position of the grid there is either a space
 // or a "#" character. The characters should form a chessboard.
 
-for() {};
+let size = 8;
+
+let board = "";
+
+for(let row=0; row<size; row++) {
+    for(let col=0; col<size; col++) {
+        if((row+col) % 2 === 0)
+            board += " ";
+        else
+            board += "#";
+    };
+    board += "\n";
+};
+
+console.log(board);
