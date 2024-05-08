d56bc2ef0437b9ff18031d54c969965e78399d55
shayaaan <42377211+shayanahmedk@users.noreply.github.com>
2024-05-08 07:04:36 +0500

    added exercises

---
+++ chapter-2/chessboard.js
@@ -0,0 +1,5 @@
+// Write a program that creates a string that represents an 8×8 grid, using newline
+// characters to separate lines. At each position of the grid there is either a space
+// or a "#" character. The characters should form a chessboard.
+
+for() {};
